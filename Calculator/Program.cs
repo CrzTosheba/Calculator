6fe9b978ae569ebcbe1765938ed6979c7de60af0
CrzTosheba <fugu@list.ru>
2022-04-10 14:32:27 +0300

    Калькулятор

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,14 +1,65 @@
 ﻿using System;
+using System.Collections;
 
 namespace Calculator
 { 
 
+
+    class Program
+    {
+   
+        public static void Main(string[] args)
+        {
+              
+            
+            Calc<int> mc1 = new Calc<int>();
+            while (true)
+            {
+                
+
+                try
+                {
+                    Console.WriteLine("Введите число 1");
+                    int Num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите число 2");
+                    int Num2 = Convert.ToInt32(Console.ReadLine());
+                    double res1 = mc1.Add(Num1, Num2);
+
+                    Console.WriteLine("Результат сложения = {0}", res1);
+                    string? str = UserMenu();
+                    Console.ReadKey();
+
+                }
+                catch (ExitException e)
+                {
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Ошибка: {e.Message}");
+                }
+
+
+            }
+           
+        }
+        private static string? UserMenu()
+        {
+
+            Console.WriteLine("Для выхода введите '-q'");
+            string? str = Console.ReadLine();
+            if (str == "-q")
+                throw new ExitException();
+            return str;
+        }
+    }
+    
     // Интерфейс, получающий параметром тип T
     interface ICalculate<T>
     {
         // Объявление методов, которые используют тип T
         double Add(T var1, T var2);
-      
+
     }
 
 
@@ -21,25 +72,8 @@ namespace Calculator
             return res;
         }
 
-      
-    }
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-           
-         
-            Calc<int> mc1 = new Calc<int>();
-        
-            Console.WriteLine("Введите число 1");
-            int Num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите число 2");
-            int Num2 = Convert.ToInt32(Console.ReadLine());
-             double res1 = mc1.Add(Num1, Num2);
-
-            Console.WriteLine("Результат сложения = {0}", res1);
-            Console.ReadKey();
-        }
     }
+ 
+
 }
