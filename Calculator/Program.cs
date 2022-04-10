814da33db8c81109a400a2488d68a0ea5a55ff3c
CrzTosheba <fugu@list.ru>
2022-04-10 16:24:06 +0300

    Калькулятор, плюс интерфейсы и изменение цвета, задание 10.5

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,7 +10,10 @@ namespace Calculator
    
         public static void Main(string[] args)
         {
-              
+            ICalcLog log = new CalcLog();
+            ICalcLog err = new CalcLog();
+           
+            
             
             Calc<int> mc1 = new Calc<int>();
             while (true)
@@ -19,34 +22,47 @@ namespace Calculator
 
                 try
                 {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine("Введите число 1");
                     int Num1 = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Введите число 2");
                     int Num2 = Convert.ToInt32(Console.ReadLine());
                     double res1 = mc1.Add(Num1, Num2);
-
-                    Console.WriteLine("Результат сложения = {0}", res1);
+                    log.Event("");
+                    Console.WriteLine("{0}", res1);
                     string? str = UserMenu();
-                    Console.ReadKey();
+                    //Console.ReadKey();
 
                 }
                 catch (ExitException e)
                 {
+                    
                     break;
+
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine($"Ошибка: {e.Message}");
+                    err.Error("");
+
                 }
 
 
             }
            
+
+
+
+
+
         }
         private static string? UserMenu()
         {
-
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Для выхода введите '-q'");
+            Console.WriteLine("Или нажмите любую клавишу для повтора");
             string? str = Console.ReadLine();
             if (str == "-q")
                 throw new ExitException();
@@ -74,6 +90,30 @@ namespace Calculator
 
 
     }
- 
+    public interface ICalcLog
+    {
+        void Event(string message);
+        
+        void Error(string message);
+
+    }
+    public class CalcLog : ICalcLog
+    {
+        public void Event(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Результат сложения");
+        }
+
+        public void Error(string message)
+        {
+            
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Ввели что-то не то ");
+        }
+    }
+
 
 }
